f6f89357c5bf88ab2a44fbf2e342830d3ad562c7
AdamCzyz <u19_adaczy_waw@technischools.com>
2023-03-14 15:57:52 +0100

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const postsApi = require('./server/posts');
+
+app.use('/', express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use(cookieParser());
+app.use(postsApi);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server up and running on port ${process.env.PORT || 8080}`);
+});
