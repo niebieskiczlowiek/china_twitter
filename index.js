c26ab6d19d410c29febadc3cad8f3689d82066d3
AdamCzyz <u19_adaczy_waw@technischools.com>
2023-03-16 14:41:18 +0100

    new front

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const postsApi = require('./server/posts');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-app.use('/', express.static(__dirname + '/public'));
+app.use("/", express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({ limit: '2mb' }));
+app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cookieParser());
-app.use(postsApi);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Server up and running on port ${process.env.PORT || 8080}`);
