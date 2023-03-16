5b2addab5aeb88988a5c0e0c92efdff1f4123a40
Olsson_Polska <94561632+Olssson@users.noreply.github.com>
2023-03-16 15:43:44 +0100

    Data Base

--- server/models/users.js
+++ server/models/users.js
@@ -1,1 +1,33 @@
-const mongo = "kongo"
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  nick: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique : true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  admin: {
+    type: Boolean,
+    default: false,
+  }
+});
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;
